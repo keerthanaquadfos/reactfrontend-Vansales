b7971ce44705bb94608b90fd1c19966b36bcff4f
unknown <abhilsahkm1988@gmail.com>
2023-07-07 18:45:46 +0530

    footer info commented

--- src/components/shared/Layout/Footer.js
+++ src/components/shared/Layout/Footer.js
@@ -6,14 +6,14 @@ function Footer() {
   <footer className="content-footer footer bg-footer-theme">
     <div className="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
       <div className="mb-2 mb-md-0">
-        ©, www.absnams.com
-        <a href="/" target="_blank" className="footer-link fw-bolder">ThemeSelection</a>
+        ©, VAN SALES LLC
+        <a href="/#" target="_blank" className="footer-link fw-bolder">VAN SALES LLC</a>
       </div>
       <div>
-        <a href="/" className="footer-link me-4" target="_blank">License</a>
+      {/*   <a href="/" className="footer-link me-4" target="_blank">License</a>
         <a href="/" target="_blank" className="footer-link me-4">More Themes</a> 
         <a href="/" target="_blank" className="footer-link me-4">Documentation</a> 
-        <a href="/" target="_blank" className="footer-link me-4">Support</a>
+        <a href="/" target="_blank" className="footer-link me-4">Support</a> */}
       </div>
     </div>
   </footer>
