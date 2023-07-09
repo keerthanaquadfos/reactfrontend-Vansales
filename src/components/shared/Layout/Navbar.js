4aa488dc43a7eeff3ea7d76b4d372ac18c4fa8b7
unknown <abhilsahkm1988@gmail.com>
2023-07-09 19:17:50 +0530

    mobile menu button cick logout issue fixed

--- src/components/shared/Layout/Navbar.js
+++ src/components/shared/Layout/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
   return <>
     <nav className="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
   <div className="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
-    <a className="nav-item nav-link px-0 me-xl-4" href="/">
+    <a className="nav-item nav-link px-0 me-xl-4" href="/#">
       <i className="bx bx-menu bx-sm" />
     </a>
   </div>
@@ -23,14 +23,14 @@ function Navbar() {
     <ul className="navbar-nav flex-row align-items-center ms-auto"> 
     
       <li className="nav-item navbar-dropdown dropdown-user dropdown">
-        <a className="nav-link dropdown-toggle hide-arrow" href="/;" data-bs-toggle="dropdown">
+        <a className="nav-link dropdown-toggle hide-arrow" href="/#" data-bs-toggle="dropdown">
           <div className="avatar avatar-online">
             <img src="../assets/img/avatars/1.png" alt="" className="w-px-40 h-auto rounded-circle" />
           </div>
         </a>
         <ul className="dropdown-menu dropdown-menu-end">
           <li>
-            <a className="dropdown-item" href="/">
+            <a className="dropdown-item" href="/#">
               <div className="d-flex">
                 <div className="flex-shrink-0 me-3">
                   <div className="avatar avatar-online">
